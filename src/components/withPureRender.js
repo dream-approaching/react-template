3e4c1251f8076ca532b56b57003511fcee5d8c8f
郑金寿 <891661163@qq.com>
2019-01-15 17:31:09 +0800

    1、shouldComponentUpdate的两种方式 2、增加官方modal组件 3、想体验react hooks的，不过最新版本还不能用

--- src/components/withPureRender.js
+++ src/components/withPureRender.js
@@ -1,14 +1,45 @@
 import { isEqual } from 'utils/utils';
+import { is } from 'immutable';
 
 const withPureRender = Component => {
   class Hoc extends Component {
     // 覆写父组件的生命周期函数
     shouldComponentUpdate(nextProps, nextState) {
+      return this.pureRenderType1(nextProps, nextState);
+      // return this.pureRenderType2(nextProps, nextState); // 两种方式
+    }
+
+    pureRenderType1 = (nextProps, nextState) => {
       if (!isEqual(this.props, nextProps) || !isEqual(this.state, nextState)) {
         return true;
       }
       return false;
-    }
+    };
+
+    pureRenderType2 = (nextProps, nextState) => {
+      const thisProps = this.props || {};
+      const thisState = this.state || {};
+
+      if (
+        Object.keys(thisProps).length !== Object.keys(nextProps).length ||
+        Object.keys(thisState).length !== Object.keys(nextState).length
+      ) {
+        return true;
+      }
+
+      for (const key in nextProps) {
+        if (!is(thisProps[key], nextProps[key])) {
+          return true;
+        }
+      }
+
+      for (const key in nextState) {
+        if (thisState[key] !== nextState[key] && !is(thisState[key], nextState[key])) {
+          return true;
+        }
+      }
+      return false;
+    };
 
     render() {
       return super.render();
--- src/pages/news/news.js
+++ src/pages/news/news.js
@@ -1,5 +1,35 @@
 import React from 'react';
+import Modal from 'components/modal/modal';
+import styles from './style.scss';
 
-export default function Component() {
-  return <div>news</div>;
+export default class extends React.Component {
+  state = {
+    showModal: false,
+  };
+
+  handleShowModal = () => {
+    this.setState({ showModal: true });
+  };
+
+  handleHideModal = () => {
+    this.setState({ showModal: false });
+  };
+
+  render() {
+    const { showModal } = this.state;
+    return (
+      <div>
+        <div onClick={this.handleShowModal}>click me to showModal</div>
+        {showModal && (
+          <Modal>
+            <div className={styles.modalCon}>
+              <div>header</div>
+              <div>body</div>
+              <div onClick={this.handleHideModal}>关闭</div>
+            </div>
+          </Modal>
+        )}
+      </div>
+    );
+  }
 }
