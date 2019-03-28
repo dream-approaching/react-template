8b5e01247e54978f4ebf9610204b4e24ec11dede
longzi <891661163@qq.com>
2019-03-28 16:20:05 +0800

    测试form组件

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import history from 'utils/history';
 import { saveLocalStorage } from 'utils/utils';
+import { Input, Button, Form, Icon } from 'antd';
 import styles from './style.scss';
 
-export default function Component() {
-  const handleLogin = () => {
+class Login extends React.Component {
+  handleLogin = () => {
     console.log(123, history);
     saveLocalStorage('user', 'login');
     history.push('/');
   };
 
-  return (
-    <div onClick={handleLogin} className={styles.btn}>
-      登录
-    </div>
-  );
+  render() {
+    const { form } = this.props;
+    const { getFieldDecorator } = form;
+    return (
+      <Fragment>
+        <Form className={styles.wrapCon} onSubmit={this.handleLogin}>
+          <Form.Item>
+            {getFieldDecorator('userName', {
+              rules: [{ required: true, message: '请输入用户名' }],
+            })(
+              <Input
+                placeholder="请输入用户名"
+                prefix={<Icon type="user" style={{ color: 'rgba(0, 0, 0, 0.25)' }} />}
+              />
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: '请输入密码' }],
+            })(
+              <Input
+                placeholder="请输入密码"
+                prefix={<Icon type="lock" style={{ color: 'rgba(0, 0, 0, 0.25)' }} />}
+              />
+            )}
+          </Form.Item>
+          <Button type="primary" htmlType="submit" className={styles.btn}>
+            登录
+          </Button>
+        </Form>
+      </Fragment>
+    );
+  }
 }
+
+const LoginWithForm = Form.create()(Login);
+
+export default LoginWithForm;
